8c7044c5bc6b7169d7dfa2a38568e87f5136222f
zzc <zhangzc>
2022-05-07 09:57:27 +0800

    提交

---
+++ TestSdk/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  TestSdk
+//
+//  Created by zzc on 2022/5/7.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ TestSdk/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  TestSdk
+//
+//  Created by zzc on 2022/5/7.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface ViewController : NSViewController
+
+
+@end
+
